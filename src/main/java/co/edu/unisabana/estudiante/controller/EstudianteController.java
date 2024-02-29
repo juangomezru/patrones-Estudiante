7f411627da2c2a5fd07f7fb3c43ea701b6f6a967
Juan Diego Gómez <128845389+juangomezru@users.noreply.github.com>
2024-02-28 20:03:15 -0500

    Update EstudianteController.java

--- src/main/java/co/edu/unisabana/estudiante/controller/EstudianteController.java
+++ src/main/java/co/edu/unisabana/estudiante/controller/EstudianteController.java
@@ -40,6 +40,7 @@ public class EstudianteController {
     public String pruebahola(@PathVariable int cantidad) {
        for (int i = 0; i < cantidad; i++){
            int x = 0;
+           System.out.println("Hola mundo");
            x++;
        }
        return variable;
